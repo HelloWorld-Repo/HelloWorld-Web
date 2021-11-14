7589e3a78e33bb72a61a6715600b787a8c639803
Gabriela Medeiros da Silva <gabriela.medeiros2010@hotmail.com.br>
2021-11-14 02:11:50 -0300

    Creating navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,25 @@
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useAuth } from "../../contexts/auth";
+
+const Navbar = () => {
+  const { user } = useAuth();
+
+  return (
+    <Box>
+      <AppBar position="static" elevation={0} color="tertiary">
+        <Toolbar>
+          <Typography flexGrow={1} fontSize={15} variant="h6" component="div">
+            Olá, {user?.name.split(" ")[0]}
+          </Typography>
+          <Button fontSize={15} color="inherit">
+            Sair
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
