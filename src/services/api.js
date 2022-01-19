55bc1cfae3c1e3d11c050f5a35c5cb7263532fca
Gabriela Medeiros da Silva <gabriela.medeiros2010@hotmail.com.br>
2022-01-19 14:39:06 -0300

    Removing prod files and updating env server

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  // baseURL: 'http://192.168.15.100:3000',
+  baseURL:
+    process.env.REACT_APP_API_URL,
   timeout: 15000,
   timeoutErrorMessage:
-    'Isso está demorando mais do que deveria, você pode tentar de novo?',
+    "Isso está demorando mais do que deveria, você pode tentar de novo?",
   headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
+    "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
 
