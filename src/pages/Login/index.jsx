60895743f4125aeb4bf5adfd9b9a69b2a70a5562
Gabriela Medeiros da Silva <gabriela.medeiros2010@hotmail.com.br>
2022-03-01 16:12:20 -0300

    Creating home wihout login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Button, Typography, useTheme } from "@mui/material";
 import AndroidIcon from "@mui/icons-material/Android";
 
@@ -8,22 +8,30 @@ import LoginForm from "./components/LoginForm";
 import useStyles from "./styles";
 
 const Login = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
   const classes = useStyles();
   const theme = useTheme();
 
   useTitle("Aprenda programação gratuitamente");
 
   return (
-    <Grid direction="row" container className={classes.gridContainer}>
+    <Grid
+      direction="row"
+      container
+      backgroundColor={theme.palette.primary.main}
+      className={classes.gridContainer}
+    >
       <Grid
         direction="column"
         justifyContent="center"
         alignItems="center"
         display="flex"
-        md={6}
+        md={isAdmin ? 6 : 12}
         xs={12}
         container
         item
+        borderRadius={isAdmin ? 0 : 50}
+        backgroundColor="#FFF"
       >
         <Logo containerClasses={classes.logoContainer} />
         <Button
@@ -34,22 +42,25 @@ const Login = () => {
         >
           Baixar Aplicativo
         </Button>
+        <Button onClick={() => setIsAdmin(true)}>Sou Administrador</Button>
       </Grid>
-      <Grid
-        className={classes.formContainer}
-        md={6}
-        xs={12}
-        direction="column"
-        container
-        item
-        padding={theme.spacing(0, 2)}
-      >
-        <Typography textAlign="center">
-          Você é administrador? Digite seu login para continuar! <br/>
-          Caso não seja, <strong>baixe o aplicativo</strong>
-        </Typography>
-        <LoginForm />
-      </Grid>
+      {isAdmin && (
+        <Grid
+          className={classes.formContainer}
+          md={6}
+          xs={12}
+          direction="column"
+          container
+          item
+          padding={theme.spacing(0, 2)}
+        >
+          <Typography textAlign="center">
+            Você é administrador? Digite seu login para continuar! <br />
+            Caso não seja, <strong>baixe o aplicativo</strong>
+          </Typography>
+          <LoginForm />
+        </Grid>
+      )}
     </Grid>
   );
 };
